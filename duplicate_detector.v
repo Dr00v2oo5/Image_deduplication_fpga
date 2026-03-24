d5a7b2075e62b982d450f9d04dc41805109c88a2
Dr00v2oo5 <bhaipdhruv@gmail.com>
2026-03-24 22:22:58 +0530

    Add files via upload
    Hamming detector and Image duplicate checking block

---
+++ duplicate_detector.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.03.2026 21:21:22
+// Design Name: 
+// Module Name: duplicate_detector
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module duplicate_detector (
+    input [63:0] sig1,
+    input [63:0] sig2,
+    input [6:0] threshold,
+    output reg duplicate,
+    output reg [6:0] distance
+);
+
+    integer i;
+    reg [63:0] diff;
+
+    always @(*) begin
+        diff = sig1 ^ sig2;
+        distance = 0;
+
+        for (i = 0; i < 64; i = i + 1) begin
+            distance = distance + diff[i];
+        end
+
+        if (distance <= threshold)
+            duplicate = 1;
+        else
+            duplicate = 0;
+    end
+
+endmodule
